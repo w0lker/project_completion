344ae336cca94811d3ce81c921f0be425ea2d991
w0lker <w0lker.tg@gmail.com>
2017-06-15 20:32:55 +0800

    修改脚本提示和说明

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
-
+cp prj $HOME/.local/bin
 local_bash_completion_d="$HOME/.local/etc/bash_completion.d"
 cp prj_completion $local_bash_completion_d/prj_completion
 cd $local_bash_completion_d
 sh do_completion_flush.sh
 cd $current_path
+
